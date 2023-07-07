0346389b5868857db5d28467933061a6dcd58b50
Adithya <adithya22@protonmail.com>
2023-07-07 13:14:54 +0530

    Added some useful pgn functions

--- pgnUtils.py
+++ pgnUtils.py
@@ -1,6 +1,8 @@
+from re import split
 import chess.pgn
+import os
 
-
+INPUT_PGN_PATH = "pgn/ytd_mipola_parsed.pgn"
 
 def getNumberOfGames(pgnPath):
     pgn = open(pgnPath)
@@ -25,5 +27,30 @@ def getGame(pgnPath,number):
     
     return None
     
-print(getNumberOfGames("pgn/ytd_mipola_parsed.pgn"))
-print(getGame("pgn/ytd_mipola_parsed.pgn",550))
+def splitPgn(pgnPath,number):
+    OUTPUT_PGN_1 = "split1.pgn"
+    OUTPUT_PGN_2 = "split2.pgn"
+    PGN_TO_WRITE_TO = OUTPUT_PGN_1
+    
+    if os.path.exists(OUTPUT_PGN_1):
+        os.remove(OUTPUT_PGN_1)
+    if os.path.exists(OUTPUT_PGN_2):
+        os.remove(OUTPUT_PGN_2)
+    
+    pgn = open(pgnPath)
+    count = 0
+    while True:
+        game = chess.pgn.read_game(pgn)
+        if not game:
+            break
+        count+=1
+        if count>number:
+            PGN_TO_WRITE_TO = OUTPUT_PGN_2
+        outputPgn = open(PGN_TO_WRITE_TO, "a", encoding="utf-8")
+        exporter = chess.pgn.FileExporter(outputPgn, comments=True, variations=True)
+        game.accept(exporter)
+        
+
+print(getNumberOfGames('split1.pgn'))
+print(getNumberOfGames('split2.pgn'))
+print(getNumberOfGames(INPUT_PGN_PATH))
